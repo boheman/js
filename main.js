d872a2265c1bbfce1d2acd3b3ae68c808aa4cf1b
Sura Pong L <5574493+boheman@users.noreply.github.com>
2019-12-19 17:43:46 -0800

    chapter 6 - WIP

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-main05();
+// main06();
+
+function speak(line) {
+  console.log(`The ${this.type} rabbit says '${line}'`);
+}
+whiteRabbit = {
+  type: 'white', // property 'type'
+  speak, // bind method 'speak' with fn value
+};
+hungryRabbit = {
+  type: 'hungry',
+  speak,
+};
+whiteRabbit.speak("ABC");
+
+let empty = {};
+console.log(empty.toString());
