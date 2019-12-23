2d49eccc1dbb01a15b6dd4d0d061cf5767778ecf
Surapong L <5574493+boheman@users.noreply.github.com>
2019-12-22 17:13:07 -0800

    chapter 09: regex WIP

--- main.js
+++ main.js
@@ -19,6 +19,11 @@ console.log(dateTime.test("01-jan-2003 15:20")); // false
 let binary = /[01]/;
 let noBinary = /[^01]/;
 
+// [^] match any char that is not empty set of char.
+let matchEverything = /[^]*/;
+console.log(/.+/.test("\n")); // false (period cannot match newline)
+console.log(/[^]+/.test("\n")); // true
+
 // Quantifier
 let oneOrMoreDigit = /\d+/;
 let zeroOrMoreDigit = /\d*/;
@@ -72,5 +77,63 @@ console.log(getDate("12-3-2019"));
 console.log(/^\d+$/.test("100")); // true
 console.log(/^\d+$/.test("s100")); // false
 
+// There cannot be an x before the start of string.
+let neverMatchAnything = /x^/; // This is likely a bug.
+
+// \b is used as word boundary
 console.log(/cat/.test("concatenate")); // true
 console.log(/\bcat\b/.test("concatenate")); // false
+
+// Choice patterns
+let animalCount = /\b\d+ (pig|cow|chicken)s?\b/;
+console.log(animalCount.test("15 pigs")); // true
+
+let binOrHexOrDec = /\b([01]+b|[\da-fA-F]+h|\d+)\b/;
+console.log(binOrHexOrDec.test("010101100b")); // true
+console.log(binOrHexOrDec.test("deadBEEFh")); // true
+console.log(binOrHexOrDec.test("1234")); // true
+
+// String Replace
+/* Only the first match got replace. */
+console.log("papa".replace("p", "m")); // mapa
+
+// Use regex instead
+console.log("papa".replace(/p/, "m")); // save as above
+
+// Globally replace (i.e. not only the first hit).
+console.log("papa".replace(/p/g, "m")); // mama
+console.log("Borobudur".replace(/[ou]/, "a")); // Barobudur
+console.log("Borobudur".replace(/[ou]/g, "a")); // Barabadar
+
+// sura => pong
+// cherry => wong
+console.log(
+  "pong, sura\nwong, cherry".replace(
+    /(\w+), (\w+)/g,
+    ("$2 => $1"))); // $1 and $2 are matched groups.
+
+let stock = "1 lemon, 2 cabbages, and 101 eggs";
+console.log(stock.replace(/(\d+) (\w+)/g, minusOne));
+// 0:lemon, 1:cabbages, and 100:eggs
+function minusOne(_, digits, words) {
+  return (digits - 1) + ":" + words;
+}
+
+// Greed
+function stripComments(code) {
+  let comment = /\/\/.*|\/\*[^]*\*\//g;
+  return code.replace(comment, "");
+}
+console.log(stripComments("x += 1; // hello there.")); // x += 1;
+console.log(stripComments("run(/* count= */ 2, true);")); // run( 2, true);
+console.log(stripComments("/**\n Helper function\n hello.\n */")); //
+
+// 1 2; Why? Greedy matching!!
+console.log(stripComments("1 /* a */ + /* b */ 2;")) // 1 2;
+
+// Non-greedy matching +? *? ?? {}?
+function nonGreedyStripComments(code) {
+  let comment = /\/\/.*|\/\*[^]*?\*\//g;
+  return code.replace(comment, "");
+}
+console.log(nonGreedyStripComments("1 /* a */ + /* b */ 2;")) // 1 + 2;
