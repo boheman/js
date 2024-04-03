bd450806a10ec78d66d4f03f6ca7ea00ef7aafe5
Sura Pong <154379662+yeeha567@users.noreply.github.com>
2024-04-03 05:09:34 +0000

    Address comment

--- testJs.js
+++ testJs.js
@@ -1,5 +1,4 @@
-// This is my first Javascript code!
 console.log("Hello World");
 
-console.log("Hello not world");
+console.log("Hell not world");
 console.log(encodeURIComponent("x=hello world"));
